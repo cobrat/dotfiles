f830aa223b2f57bd18807635bb3fcd8e152c99e9
wzh <zihan.wu042@gmail.com>
2026-01-12 17:36:14 +0800

    fix: change astornvim c/cpp file indentation and tab width

--- nvim/lua/plugins/clangd.lua
+++ nvim/lua/plugins/clangd.lua
@@ -2,6 +2,18 @@ return {
   {
     "AstroNvim/astrocore",
     opts = {
+      autocmds = {
+        c_indent = {
+          {
+            event = "FileType",
+            pattern = { "c", "cpp" },
+            callback = function()
+              vim.opt_local.shiftwidth = 4
+              vim.opt_local.tabstop = 4
+            end,
+          },
+        },
+      },
       filetypes = {
         extension = {
           h = "c",
