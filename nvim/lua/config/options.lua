a8b506e5d1fe2c842b3eb9501de8be2314c8fc13
wzh <zihan.wu042@gmail.com>
2026-01-13 21:21:21 +0800

    fix: lazyvim customization

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Snacks animations
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4
