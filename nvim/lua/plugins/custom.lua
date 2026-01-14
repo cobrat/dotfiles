43d754c3c241b9134fc10f6c366d3c0cdcfda5fc
wzh <zihan.wu042@gmail.com>
2026-01-14 11:51:35 +0800

    feat: add cmp custimization for lazyvim

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -11,4 +11,32 @@ return {
             inlay_hints = { enabled = false },
         },
     },
+    {
+        "saghen/blink.cmp",
+        opts = {
+            keymap = {
+                preset = "none",
+                ["<CR>"] = { "fallback" },
+                ["<Tab>"] = { "select_and_accept", "fallback" },
+                -- ["<C-space>"] = { "show", "show_documentation", "hide" },
+                ["<C-e>"] = { "hide" },
+                ["<C-p>"] = { "select_prev", "fallback" },
+                ["<C-n>"] = { "select_next", "fallback" },
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+            },
+            completion = {
+                ghost_text = { enabled = false },
+                list = {
+                    selection = {
+                        preselect = false,
+                        auto_insert = false,
+                    },
+                },
+                menu = {
+                    auto_show = true,
+                },
+            },
+        },
+    },
 }
