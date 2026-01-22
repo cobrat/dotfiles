a6f6d4bad4d6bee85079ca762ec324d33b73b8d9
wzh <zihan.wu042@gmail.com>
2026-01-22 19:49:49 +0800

    refactor: start a new custom neovim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,17 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
+-- Reference to keymap function
 local keymap = vim.keymap
 
-keymap.set("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
-keymap.set("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
+-- Save file
+keymap.set("n", "<leader>w", ":w<cr>")
+
+-- Quit
+keymap.set("n", "<leader>q", ":q<cr>")
+
+-- Clear search highlight
+keymap.set("n", "<leader>h", ":nohlsearch<cr>")
+
+-- Move between windows
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-l>", "<C-w>l")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,12 +1,29 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Set the leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Snacks animations
-vim.g.snacks_animate = false
-
+-- Reference to options
 local opt = vim.opt
 
-opt.shiftwidth = 4 -- Size of an indent
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Indentation
+opt.expandtab = true
+opt.shiftwidth = 4
 opt.tabstop = 4
-opt.wrap = true -- Disable line wrap
+opt.smartindent = true
+
+-- UI and display
+opt.termguicolors = true
+opt.cursorline = true
+opt.scrolloff = 8
+opt.mouse = "a"
+
+-- System integration
+opt.clipboard = "unnamedplus"
+
+-- Search behavior
+opt.ignorecase = true
+opt.smartcase = true
