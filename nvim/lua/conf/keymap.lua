b84ca8c99d0bdde9875e5a79531b91c2b3dff47f
wzh <zihan.wu042@gmail.com>
2026-02-23 21:50:29 +0800

    feat: add neovim custom config

--- nvim/lua/conf/keymap.lua
+++ nvim/lua/conf/keymap.lua
@@ -59,7 +59,7 @@ keymap("x", "p", '"_dP', opts)
 -- Terminal mode --
 
 -- Terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
