24db22bb36cbb774004dfc94f57de3b9626d1fab
wzh <zihan.wu042@gmail.com>
2026-03-27 10:23:10 +0800

    fix: fix fish config format

--- fish/config.fish
+++ fish/config.fish
@@ -43,22 +43,28 @@ if status is-interactive
         set -e FZF_ALT_C_OPTS
 
         if command -q fd
-            set -gx FZF_DEFAULT_COMMAND "fd --hidden --strip-cwd-prefix --exclude .git"
-            set -gx FZF_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+            set -gx FZF_DEFAULT_COMMAND \
+                "fd --hidden --strip-cwd-prefix --exclude .git"
+            set -gx FZF_ALT_C_COMMAND \
+                "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
         end
 
-        set -gx FZF_DEFAULT_OPTS "--height 50% --layout=default --border --color=hl:#2dd4bf"
+        set -gx FZF_DEFAULT_OPTS \
+            "--height 50% --layout=default --border --color=hl:#2dd4bf"
 
         if command -q bat
-            set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always -n --line-range :500 {}'"
+            set -gx FZF_CTRL_T_OPTS \
+                "--preview 'bat --color=always -n --line-range :500 {}'"
         else
             set -gx FZF_CTRL_T_OPTS "--preview 'sed -n \"1,500p\" {}'"
         end
 
         if command -q eza
-            set -gx FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
+            set -gx FZF_ALT_C_OPTS \
+                "--preview 'eza --tree --color=always {} | head -200'"
         else
-            set -gx FZF_ALT_C_OPTS "--preview 'find {} -maxdepth 3 -print | head -200'"
+            set -gx FZF_ALT_C_OPTS \
+                "--preview 'find {} -maxdepth 3 -print | head -200'"
         end
 
         fzf --fish | source
@@ -70,11 +76,13 @@ if status is-interactive
         alias la="eza -la --color=always --no-user"
 
         function tree
-            eza --tree --level=3 --all --ignore-glob='.git' --color=always $argv
+            eza --tree --level=3 --all --ignore-glob='.git' \
+                --color=always $argv
         end
 
         function dtree
-            eza --tree --level=3 --all --only-dirs --ignore-glob='.git' --color=always $argv
+            eza --tree --level=3 --all --only-dirs \
+                --ignore-glob='.git' --color=always $argv
         end
     end
 
