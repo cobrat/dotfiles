7f87c07ba1e5b7ec0b831fd1c35d0d70c3c05c89
wzh <zihan.wu042@gmail.com>
2026-05-05 12:05:05 +0800

    fix(zsh): guard interactive startup

--- zsh/extra/tmux-title.zsh
+++ zsh/extra/tmux-title.zsh
@@ -13,6 +13,29 @@ _dotfiles_title_sanitize() {
   print -rn -- "$s"
 }
 
+_dotfiles_title_urlencode_path() {
+  emulate -L zsh
+  local LC_ALL=C
+  local input=$1
+  local output='' char byte
+  local -i i
+
+  for (( i = 1; i <= ${#input}; i++ )); do
+    char=${input[i]}
+    case $char in
+      [A-Za-z0-9.~_/-])
+        output+=$char
+        ;;
+      *)
+        printf -v byte '%%%02X' "'$char"
+        output+=$byte
+        ;;
+    esac
+  done
+
+  print -rn -- "$output"
+}
+
 _dotfiles_title_precmd() {
   emulate -L zsh
   local host="${(%):-%m}"
@@ -20,8 +43,10 @@ _dotfiles_title_precmd() {
   local idle="${USER}@${host}: ${dir}"
   printf '\033]2;%s\033\\' "$( _dotfiles_title_sanitize "$idle" )"
 
-  if [[ ${TERM_PROGRAM:-} == Apple_Terminal && -z ${TMUX:-} ]]; then
-    printf '\e]7;%s\a' "$PWD"
+  # OSC 7: report cwd so Ghostty/kitty can inherit it for new panes/tabs.
+  # Skipped inside tmux (tmux does not forward OSC 7 to the outer terminal).
+  if [[ -z ${TMUX:-} ]]; then
+    printf '\e]7;file://%s%s\a' "${HOST}" "$( _dotfiles_title_urlencode_path "$PWD" )"
   fi
 }
 
